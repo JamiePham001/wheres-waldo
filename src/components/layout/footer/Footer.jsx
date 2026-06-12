8a4fa12a16d9f92dcb8a6374e5dee29810977fbf
Jamie Pham <jamie.pham@outlook.com>
2026-06-12 16:46:03 +0800

    prerendering error fix
    Next.js fails while prerendering because <Html> from next/document is being imported outside of pages/_document. See the Netlify log entries at line 75 and line 79, which report "Error: <Html> should not be imported outside of pages/_document.". This causes the build to exit with code 1.

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -3,7 +3,7 @@ import style from "./style.module.css";
 export default function Footer() {
   return (
     <footer className={style.footer}>
-      <p>© 2026 Where's Waldo? All rights reserved.</p>
+      <p>© 2026 Where&apos;s Waldo? All rights reserved.</p>
     </footer>
   );
 }
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <header className={styles.header}>
       <Link href="/">
         <h1 style={{ display: "flex", gap: "10px" }}>
-          <div style={{ color: "blue" }}>Where's</div>
+          <div style={{ color: "blue" }}>Where&apos;s</div>
           <div style={{ color: "red" }}>Waldo?</div>
           <div style={{ color: "white" }}>Online</div>
         </h1>
